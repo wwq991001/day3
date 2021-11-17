f74fb94bc8a880ced10098a86f12efa4c5c608ee
wwq991001 <88713028+wwq991001@users.noreply.github.com>
2021-11-17 15:36:41 +0800

    Delete autoweb03【自动化框架】 directory

--- autoweb03【自动化框架】/GoogleChrome.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-def Chrome():
-    option = webdriver.ChromeOptions
-    option.binary_location = r'D:\Google\Chrome\Application\chrome.exe'
